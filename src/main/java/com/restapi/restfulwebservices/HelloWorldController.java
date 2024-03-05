8c9d1f0a9600d68da60cc6ae7ccf424332b3e9fc
Md Imon Bhuiya <imonbhuiya2@gmail.com>
2024-03-05 21:09:03 +0100

    Enhancing the Hello World REST API to return a Bean

--- src/main/java/com/restapi/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/restapi/restfulwebservices/HelloWorldController.java
@@ -14,4 +14,12 @@ public class HelloWorldController {
     public String helloWorld() {
         return "Hello World";
     }
+
+
+
+    @RequestMapping(method = RequestMethod.GET,path ="/hello-world-bean" )
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
 }
